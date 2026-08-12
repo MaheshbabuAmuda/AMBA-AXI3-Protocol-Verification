8037a83f1a67917f87169ea04eec81eb7291ba31
MaheshbabuAmuda <87308339+MaheshbabuAmuda@users.noreply.github.com>
2026-08-12 22:44:14 +0530

    master_agent_top
    Refactor master_agent_top to use a single master_agent instance instead of a dynamic array.

--- Master_UVC/master_agent_top.sv
+++ Master_UVC/master_agent_top.sv
@@ -2,14 +2,13 @@ class master_agent_top extends uvm_env;
 
         `uvm_component_utils(master_agent_top)
 
-        master_agent m_agnth[];
+        master_agent m_agnth;
         Axi3_env_config env_cfg;
 
 extern function new(string name = "master_agent_top", uvm_component parent);
 extern function void build_phase(uvm_phase phase);
 
 endclass
-
 
 function master_agent_top::new(string name = "master_agent_top", uvm_component parent);
         super.new(name, parent);
@@ -20,13 +19,10 @@ function void master_agent_top::build_phase(uvm_phase phase);
 
          // Get environment configuration
     if (!uvm_config_db#(Axi3_env_config)::get(this, "", "Axi3_env_config", env_cfg))
-        `uvm_fatal(get_type_name(),"Failed to get Axi3_env_config from config_db")
-
-
-          // Allocate the dynamic array
-          m_agnth = new[env_cfg.no_of_master];
-
-        foreach(m_agnth[i]) begin
-        m_agnth[i] = master_agent::type_id::create($sformatf("m_agnth[%0d]",i), this);
+        begin
+            `uvm_fatal(get_type_name(),"Failed to get Axi3_env_config from config_db")
         end
+        
+        m_agnth = master_agent::type_id::create("m_agnth", this);
+
 endfunction
