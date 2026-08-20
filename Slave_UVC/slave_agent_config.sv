eefecc64902b6122368829b11829e524da348aa4
MaheshbabuAmuda <87308339+MaheshbabuAmuda@users.noreply.github.com>
2026-08-20 18:18:15 +0530

    slave_agent_config

--- Slave_UVC/slave_agent_config.sv
+++ Slave_UVC/slave_agent_config.sv
@@ -6,13 +6,11 @@ virtual Axi3_if vif;
 
 uvm_active_passive_enum is_active = UVM_ACTIVE;
 
-//transaction count
-int slv_xtn_cnt = 0;
-
 extern function new(string name = "slave_agent_config");
 
 endclass: slave_agent_config
 
 function slave_agent_config:: new(string name = "slave_agent_config");
         super.new(name);
-endfunction
+endfunction: new
+
