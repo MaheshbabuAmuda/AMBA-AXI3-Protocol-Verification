566788ca2420f16fd57649860ac2a5717c1850a6
MaheshbabuAmuda <87308339+MaheshbabuAmuda@users.noreply.github.com>
2026-08-20 18:12:41 +0530

    master_agent_config
    Removed transaction count variable from master_agent_config.

--- Master_UVC/master_agent_config.sv
+++ Master_UVC/master_agent_config.sv
@@ -6,9 +6,6 @@ virtual Axi3_if vif;
 
 uvm_active_passive_enum is_active = UVM_ACTIVE;
 
-//transaction count
-int mstr_xtn_cnt = 0;
-
 extern function new(string name = "master_agent_config");
 
 endclass: master_agent_config
@@ -16,4 +13,5 @@ endclass: master_agent_config
 
 function master_agent_config::new(string name = "master_agent_config");
         super.new(name);
-endfunction
+endfunction: new
+
