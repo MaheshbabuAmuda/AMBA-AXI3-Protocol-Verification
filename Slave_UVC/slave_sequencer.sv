bdd9e6860b7bd9eb4b72e9ae267b0b5cef145086
MaheshbabuAmuda <87308339+MaheshbabuAmuda@users.noreply.github.com>
2026-08-20 18:19:25 +0530

    slave_sequencer

---
+++ Slave_UVC/slave_sequencer.sv
@@ -0,0 +1,11 @@
+class slave_sequencer extends uvm_sequencer #(Axi3_trans);
+
+        `uvm_component_utils(slave_sequencer)
+
+        extern function new(string name = "slave_sequencer", uvm_component parent);
+
+endclass
+
+function slave_sequencer::new(string name = "slave_sequencer", uvm_component parent);
+        super.new(name, parent);
+endfunction: new
