b9c937f7b3c890ed3a2afa3d626d1cd0b8d9d50f
MaheshbabuAmuda <87308339+MaheshbabuAmuda@users.noreply.github.com>
2026-08-20 18:01:56 +0530

    master_monitor

--- Master_UVC/master_monitor.sv
+++ Master_UVC/master_monitor.sv
@@ -8,7 +8,7 @@ class master_monitor extends uvm_monitor;
 
         uvm_analysis_port #(Axi3_trans) monitor_port;
 
-        Axi3_trans xtn1, xtn_wr, xtn_rd;
+        Axi3_trans xtn_wr, xtn_rd;
 
         Axi3_trans q1[$], q2[$], q3[$];
         
@@ -175,54 +175,54 @@ endtask: collect_awaddr
 
 task master_monitor::collect_wdata(Axi3_trans xtn_wr);
 
-    int i;
+        int i;
 
-    $display("Monitoring master WDATA channel");
+        $display("Monitoring master WDATA channel");
 
-    xtn1 = Axi3_trans::type_id::create("xtn1");
+        xtn_wr.WDATA = new[xtn_wr.AWLEN + 1];
+        xtn_wr.WSTRB = new[xtn_wr.AWLEN + 1];
 
-    // Copy AW transaction
-    xtn1 = xtn_wr;
+        for (i = 0; i <= xtn_wr.AWLEN; i++) begin
 
-    // Calculate write addresses
-    xtn1.cal_addr();
+                @(vif.mstr_mon_cb iff (vif.mstr_mon_cb.WVALID && vif.mstr_mon_cb.WREADY));
+                //iff -  Wait for a clocking block event and proceed only when both WVALID and WREADY
 
-    xtn_wr.WDATA = new[xtn_wr.AWLEN + 1];
-    xtn_wr.WSTRB = new[xtn_wr.AWLEN + 1];
+                xtn_wr.WID      = vif.mstr_mon_cb.WID;
+                xtn_wr.WDATA[i] = vif.mstr_mon_cb.WDATA;
+                xtn_wr.WSTRB[i] = vif.mstr_mon_cb.WSTRB;
 
-     foreach (xtn1.WDATA[i]) begin
+                if (i == xtn_wr.AWLEN)
+                    xtn_wr.WLAST = vif.mstr_mon_cb.WLAST;
 
-        @(vif.mstr_mon_cb iff (vif.mstr_mon_cb.WVALID && vif.mstr_mon_cb.WREADY));
-        //iff -  Wait for a clocking block event and proceed only when both WVALID and WREADY
+         end
 
-        xtn_wr.WID      = vif.mstr_mon_cb.WID;
-        xtn_wr.WDATA[i] = vif.mstr_mon_cb.WDATA;
-        xtn_wr.WSTRB[i] = vif.mstr_mon_cb.WSTRB;
-
-        if (i == xtn_wr.AWLEN)
-            xtn_wr.WLAST = vif.mstr_mon_cb.WLAST;
-
-    end
-
-    `uvm_info(get_type_name(),$sformatf("From Mstr_Mon: Collected WDATA:\n%s", xtn_wr.sprint()),
-              UVM_MEDIUM)
+        `uvm_info(get_type_name(),$sformatf("From Mstr_Mon: Collected WDATA:\n%s", xtn_wr.sprint()), UVM_MEDIUM)
 
     $display("End of master monitor WDATA channel");
 
-endtask
+endtask: collect_wdata
+
+//===========================================================
+    //Monitor B channel
+//===========================================================
 
 task master_monitor::collect_bresp(Axi3_trans xtn_wr);
 
-    int bid;
-    Axi3_trans completed_xtn;
-    $display("Monitoring master BRESP channel");
+/*   int bid;
+    Axi3_trans completed_xtn; */
+        
+     $display("Monitoring master BRESP channel");
 
-    @(vif.mstr_mon_cb iff (vif.mstr_mon_cb.BVALID && vif.mstr_mon_cb.BREADY));
+        @(vif.mstr_mon_cb iff (vif.mstr_mon_cb.BVALID && vif.mstr_mon_cb.BREADY));
 
-    xtn_wr.BID   = vif.mstr_mon_cb.BID;
-    xtn_wr.BRESP = vif.mstr_mon_cb.BRESP;
+        xtn_wr.BID   = vif.mstr_mon_cb.BID;
+        xtn_wr.BRESP = vif.mstr_mon_cb.BRESP;
 
-        bid = vif.mstr_mon_cb.BID;
+        monitor_port.write(xtn_wr);
+
+        `uvm_info("MASTER_MONITOR",$sformatf("COMPLETE WRITE RESP:\n%s",xtn_wr.sprint()),UVM_MEDIUM)
+
+/*        bid = vif.mstr_mon_cb.BID;
 // Find transaction using BID
     sem_wr_table.get(1);
 
@@ -240,43 +240,45 @@ task master_monitor::collect_bresp(Axi3_trans xtn_wr);
 
         completed_xtn.BRESP = vif.mstr_mon_cb.BRESP;
 
-    //monitor_port.write(xtn_wr);
-   // m_cfg.mstr_xtn_cnt++;
-
-         // Send complete transaction
+        // Send complete transaction
         monitor_port.write(completed_xtn);
-        m_cfg.mstr_xtn_cnt++;
         `uvm_info("MASTER_MONITOR",$sformatf("COMPLETE WRITE:\n%s",completed_xtn.sprint()),UVM_MEDIUM)
 
         // Delete completed transaction
         write_outstanding.delete(bid);
     end
 
-    sem_wr_table.put(1);
+        sem_wr_table.put(1); */
 
     $display("End of master monitor BRESP channel");
 
 endtask
+                
+//===========================================================
+    //Monitor AR channel
+//===========================================================
 
 task master_monitor::collect_raddr(Axi3_trans xtn_rd);
 
   //   int arid;
 
-    $display("Monitoring master RADDR channel");
+   $display("Monitoring master RADDR channel");
 
-    @(vif.mstr_mon_cb iff (vif.mstr_mon_cb.ARVALID && vif.mstr_mon_cb.ARREADY));
+        @(vif.mstr_mon_cb iff (vif.mstr_mon_cb.ARVALID && vif.mstr_mon_cb.ARREADY));
 
-    xtn_rd.ARVALID = vif.mstr_mon_cb.ARVALID;
-    xtn_rd.ARADDR  = vif.mstr_mon_cb.ARADDR;
-    xtn_rd.ARSIZE  = vif.mstr_mon_cb.ARSIZE;
-    xtn_rd.ARID    = vif.mstr_mon_cb.ARID;
-    xtn_rd.ARLEN   = vif.mstr_mon_cb.ARLEN;
-    xtn_rd.ARBURST = vif.mstr_mon_cb.ARBURST;
+        xtn_rd.ARVALID = vif.mstr_mon_cb.ARVALID;
+        xtn_rd.ARADDR  = vif.mstr_mon_cb.ARADDR;
+        xtn_rd.ARSIZE  = vif.mstr_mon_cb.ARSIZE;
+        xtn_rd.ARID    = vif.mstr_mon_cb.ARID;
+        xtn_rd.ARLEN   = vif.mstr_mon_cb.ARLEN;
+        xtn_rd.ARBURST = vif.mstr_mon_cb.ARBURST;
+
 //      arid = xtn_rd.ARID;
-    q3.push_back(xtn_rd);
+        
+        q3.push_back(xtn_rd);
 
-     // Store in outstanding table
-/*    sem_rd_table.get(1);
+/*   // Store in outstanding table
+    sem_rd_table.get(1);
 
     if (read_outstanding.exists(arid)) begin
         `uvm_error("MASTER_MONITOR",$sformatf("Duplicate outstanding ARID=%0d", arid))
@@ -288,51 +290,44 @@ task master_monitor::collect_raddr(Axi3_trans xtn_rd);
 
     sem_rd_table.put(1);
 */
-//    monitor_port.write(xtn_rd);
-//    m_cfg.mstr_xtn_cnt++;
-
     `uvm_info(get_type_name(),$sformatf("From Mstr_Mon: Collected RADDR:\n%s",xtn_rd.sprint()), UVM_MEDIUM)
 
     $display("End of master monitor RADDR channel");
 
 endtask
 
+//===========================================================
+    //Monitor R channel
+//===========================================================
+
 task master_monitor::collect_rdata(Axi3_trans xtn_rd);
 
     $display("Monitoring master RDATA channel");
 
-    xtn_rd.RDATA = new[xtn_rd.ARLEN + 1];
-    xtn_rd.RRESP = new[xtn_rd.ARLEN + 1];
+     xtn_rd.RDATA = new[xtn_rd.ARLEN+1];
+     xtn_rd.RRESP = new[xtn_rd.ARLEN+1];
 
-    for (int i = 0; i <= xtn_rd.ARLEN; i++) begin
+    $display("Monitoring master RDATA channel");
 
-        @(vif.mstr_mon_cb iff (vif.mstr_mon_cb.RVALID && vif.mstr_mon_cb.RREADY));
+    for (int i = 0; i < xtn_rd.RDATA.size(); i++) begin
 
-        xtn_rd.RID      = vif.mstr_mon_cb.RID;
-        xtn_rd.RVALID   = vif.mstr_mon_cb.RVALID;
-        xtn_rd.RREADY   = vif.mstr_mon_cb.RREADY;
-        xtn_rd.RDATA[i] = vif.mstr_mon_cb.RDATA;
-        xtn_rd.RRESP[i] = vif.mstr_mon_cb.RRESP;
+            @(vif.mstr_mon_cb iff (vif.mstr_mon_cb.RVALID && vif.mstr_mon_cb.RREADY));
 
-        if (i == xtn_rd.ARLEN)
-            xtn_rd.RLAST = vif.mstr_mon_cb.RLAST;
+            xtn_rd.RID      = vif.mstr_mon_cb.RID;
+            xtn_rd.RVALID   = vif.mstr_mon_cb.RVALID;
+            xtn_rd.RREADY   = vif.mstr_mon_cb.RREADY;
+            xtn_rd.RDATA[i] = vif.mstr_mon_cb.RDATA;
+            xtn_rd.RRESP[i] = vif.mstr_mon_cb.RRESP;
 
-        $display("RID   = %0d", vif.mstr_mon_cb.RID);
-        $display("RDATA = %0h", vif.mstr_mon_cb.RDATA);
-        $display("RRESP = %0d", vif.mstr_mon_cb.RRESP);
-        $display("RLAST = %0b", vif.mstr_mon_cb.RLAST);
-
+            if (i == xtn_rd.ARLEN) begin
+                    xtn_rd.RLAST = vif.mstr_mon_cb.RLAST;
+            end
+            
     end
 
-    //=======================================================
-    // Send complete READ transaction
-    //=======================================================
+        monitor_port.write(xtn_rd);
 
-    monitor_port.write(xtn_rd);
-
-    m_cfg.mstr_xtn_cnt++;
-
-    `uvm_info("MASTER_MONITOR", $sformatf("COMPLETE READ:\n%s",xtn_rd.sprint()),UVM_MEDIUM)
+       `uvm_info("MASTER_MONITOR", $sformatf("COMPLETE READ:\n%s",xtn_rd.sprint()),UVM_MEDIUM)
 
     $display("MASTER MONITOR: End RDATA channel");
 
